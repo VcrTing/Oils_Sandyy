7b94e1a015b50d57db5b36684493c356a6c13984
VcrTing <vcrting@163.com>
2022-10-04 03:27:53 +0800

    0

--- oils/src/conf.js
+++ oils/src/conf.js
@@ -1,7 +1,7 @@
 
-const TEST = true
-const VERSION = '2.78'
-const VERSION_TIME = '2022-09-15' 
+const TEST = false
+const VERSION = '2.79'
+const VERSION_TIME = '2022-10-04' 
 
 const baseURL = 'https://strapi03.svr.up5d.com'
 const apiURL = baseURL
